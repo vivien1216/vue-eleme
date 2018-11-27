550d66d14f3288d050ab2003d9281f0d43210266
vivien <[1870495688@qq.com](mailto:1870495688@qq.com)>
2018-11-27 14:20:11 +0800

    'project-header'

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'style/reset.css'
+import 'style/iconfont.css'
+import 'style/index.styl'
 
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/Home/home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <home-header :seller="seller"></home-header>
+  </div>
+</template>
+
+<script>
+import homeHeader from '../../components/Header/header'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  components: {
+    homeHeader
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/seller.json')
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if(res.ret && res.data) {
+       this.seller = res.data
+       console.log(this.seller)
+      }
+    }
+  },
+  mounted () {
+    this.getHomeInfo();
+  }
+}
+</script>
+
+
+<style scoped>
+</style>
